4d30cc5604ec0f27004d5121716500c634b938df
aleksandra-zablocka <aleksandra-zablocka@o2.pl>
2023-02-28 14:52:31 +0000

    Deploying to gh-pages from @ aleksandra-zablocka/ice-cream-project-1@98306f76eea14699b8b7c2ba6383b036512ca8ef 🚀

---
+++ index.cc9ac30a.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-readmore-open]"),closeModalBtn:document.querySelector("[data-modal-readmore-close]"),modal:document.querySelector("[data-modal-readmore]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.cc9ac30a.js.map
